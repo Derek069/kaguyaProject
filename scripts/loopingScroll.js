7696dfc78a0349c7b6e6557ead9b3727f866225b
Derek069 <a6520150022@utch.edu.mx>
2022-06-22 14:00:36 -0600

    Added the css challenge and an infinite scroll test

---
+++ scripts/loopingScroll.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoader", function(){
+    var div = document.getElementById("container");
+    div.addEventListener("scroll",function(){
+        var max_scroll = this.scrollHeight - this.clientHeight;
+        var current_scroll = this.scrollTop;
+        var bottom = 100;
+        if (current_scroll + bottom >= max_scroll){
+            var
+        }
+    })
+})
